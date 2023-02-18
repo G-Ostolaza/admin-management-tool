f3763cb495eec0bdb0eddc9977168cd128ad0813
Gabriel Ostolaza <Gabe.Osto@Protonmail.com>
2023-02-18 09:50:56 -0800

    added .then to axios

--- form_crud/src/components/Adduser.js
+++ form_crud/src/components/Adduser.js
@@ -12,6 +12,8 @@ export default function SignupForm() {
             firstName,
             lastName,
             checkbox
+        }).then(()=>{
+            history.push/userinfo
         })
     }
     return (
