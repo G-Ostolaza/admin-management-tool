e4c87bf829a51233f75bc0d8c8c63396a00bdac1
Gabriel Ostolaza <Gabe.Osto@Protonmail.com>
2023-02-18 22:00:36 -0800

    changes import of useHistory to useNavifgate

--- form_crud/src/components/Adduser.js
+++ form_crud/src/components/Adduser.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
-import { useHistory } from 'react-router';
+import { useNavigate } from 'react-router-dom'
 
 export default function SignupForm() {
+    let navigate = useNavigate();
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [checkbox, setCheckbox] = useState(false);
@@ -12,8 +13,9 @@ export default function SignupForm() {
             firstName,
             lastName,
             checkbox
-        }).then(()=>{
-            history.push('/userinfo')
+        })
+        .then(()=>{
+            navigate('/userinfo')
         })
     }
     return (
